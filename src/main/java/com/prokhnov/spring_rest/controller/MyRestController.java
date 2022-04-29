18d42bc5ad99d8eb87f8caa07771b30fb91ebe99
WODweqFS <prohnovigor11@gmail.com>
2022-04-29 19:12:13 +0300

    Update My rest controller

--- src/main/java/com/prokhnov/spring_rest/controller/MyRestController.java
+++ src/main/java/com/prokhnov/spring_rest/controller/MyRestController.java
@@ -1,12 +1,9 @@
 package com.prokhnov.spring_rest.controller;
 
 import com.prokhnov.spring_rest.entity.Employee;
-import com.prokhnov.spring_rest.exception_handling.EmployeeIncorrectData;
 import com.prokhnov.spring_rest.exception_handling.NotSuchEmployeeException;
 import com.prokhnov.spring_rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +31,33 @@ public class MyRestController {
         }
 
         return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+        employee.setId(0);
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+
+        Employee employee = employeeService.getEmployee(employeeId);
+        if (employee == null){
+            throw new NotSuchEmployeeException("There is no employee with id = " + employeeId);
+        }
+
+        employeeService.deleteEmployee(employeeId);
+        return "Delete employee with id - " + employeeId;
     }
 
 }
